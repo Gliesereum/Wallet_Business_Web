933804e42b45460bef52e399e123c9c4b1326dfe
Roman Suvorov <roma232789@gmail.com>
2019-07-16 14:28:22 +0300

    add navigation btns for worker info component

--- src/components/WorkerInfo/index.jsx
+++ src/components/WorkerInfo/index.jsx
@@ -1,22 +1,70 @@
 import React, { Component } from 'react';
+import bem from 'bem-join';
+
+import {
+  Row,
+  Col,
+  Icon, Button,
+} from 'antd';
+
+import './index.scss';
+
+const b = bem('workerInfo');
 
 class WorkerInfo extends Component {
   state = {
     readOnlyMode: !this.props.isAddMode,
   };
 
+  handleUpdateWorker = () => {
+    console.log('handleUpdateWorker');
+  };
+
   render() {
+    const { changeActiveWorker } = this.props;
     const { readOnlyMode } = this.state;
 
     return (
-      <div>
-        {
-          readOnlyMode ? (
-            <div>WorkingInfoReadOnly</div>
-          ) : (
-            <div>WorkingForm</div>
-          )
-        }
+      <div className={b()}>
+        <div className={b('content')}>
+          {
+            readOnlyMode ? (
+              <div>WorkingInfoReadOnly</div>
+            ) : (
+              <div>WorkingForm</div>
+            )
+          }
+          <Row
+            gutter={40}
+            className={b('content-controlBtns')}
+          >
+            <Col lg={8}>
+              <Button
+                className={b('content-controlBtns-btn backBtn')}
+                onClick={changeActiveWorker(null, false)}
+              >
+                <Icon type="left" />
+                К списку рабочих мест
+              </Button>
+            </Col>
+            <Col lg={8}>
+              <Button
+                className={b('content-controlBtns-btn deleteBtn')}
+              >
+                Інфо блок
+              </Button>
+            </Col>
+            <Col lg={8}>
+              <Button
+                className={b('content-controlBtns-btn')}
+                type="primary"
+                onClick={this.handleUpdateWorker}
+              >
+                Сохранить
+              </Button>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
