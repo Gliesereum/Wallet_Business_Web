24cc6fdb63e9c8aefe688203ed4af0f3bbb54142
Roman Suvorov <roma232789@gmail.com>
2019-07-29 17:46:08 +0300

    create WorkerInfoDrawer dir and files inside

---
+++ src/components/WorkerInfoDrawer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import bem from 'bem-join';
+
+const b = bem('workerInfoDrawer');
+
+const WorkerInfoDrawer = () => (
+  <div className={b()}>WorkerInfoDrawer</div>
+);
+
+export default WorkerInfoDrawer;
