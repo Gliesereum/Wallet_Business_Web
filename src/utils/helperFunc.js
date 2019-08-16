e0bf5b3f9918b13cd35ce13a34f72f60d846ce61
Roman Suvorov <roma232789@gmail.com>
2019-08-16 14:57:51 +0300

    fix timezone for date and time

--- src/utils/helperFunc.js
+++ src/utils/helperFunc.js
@@ -11,15 +11,15 @@ export const getDate = (date, inHours = false) => {
   const dateInMS = new Date(date);
 
   if (inHours) {
-    const hh = String(dateInMS.getHours()).padStart(2, '0');
-    const mm = String(dateInMS.getMinutes()).padStart(2, '0');
+    const hh = String(dateInMS.getUTCHours()).padStart(2, '0');
+    const mm = String(dateInMS.getUTCMinutes()).padStart(2, '0');
 
     return `${hh}:${mm}`;
   }
 
-  const YYYY = dateInMS.getFullYear();
-  const MM = String(dateInMS.getMonth() + 1).padStart(2, '0'); // month of the year
-  const DD = String(dateInMS.getDate()).padStart(2, '0'); // day of the month
+  const YYYY = dateInMS.getUTCFullYear();
+  const MM = String(dateInMS.getUTCMonth() + 1).padStart(2, '0'); // month of the year
+  const DD = String(dateInMS.getUTCDate()).padStart(2, '0'); // day of the month
 
   return `${DD}.${MM}.${YYYY}`;
 };
