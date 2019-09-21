9e6914e68af090271e33919d0301a3be8c0df945
Chabanov Pavel <businesseuropolicy@gmail.com>
2019-09-21 18:45:32 +0300

    Add to signIn FutureBox Applications

--- src/screen/SignIn/index.jsx
+++ src/screen/SignIn/index.jsx
@@ -9,7 +9,7 @@ import {
   Row,
   Col,
   notification,
-  Steps,
+  Steps, Icon,
 } from 'antd';
 
 import { SignInForm } from '../../components/Forms';
@@ -203,6 +203,38 @@ class SignIn extends Component {
                   />
                 </Steps>
               </div>
+              <div className={b('future-box')}>
+                <div className={b('future-title')}>
+                  <h1>{phrases['signIn.futureBox.title'][defaultLanguage.isoKey]}</h1>
+                </div>
+                <div className={b('box-content-wrapper')}>
+                  <div>
+                    <FutureBoxCard
+                      title={phrases['signIn.futureBox.card.clientApp.title'][defaultLanguage.isoKey]}
+                      description={phrases['signIn.futureBox.card.clientApp.description'][defaultLanguage.isoKey]}
+                      imgSrc={phrases['signIn.futureBox.card.clientApp.imageLink'][defaultLanguage.isoKey]}
+                      appleLink={phrases['signIn.futureBox.card.clientApp.appStoreLink'][defaultLanguage.isoKey]}
+                      googleLink={phrases['signIn.futureBox.card.clientApp.playMarketLink'][defaultLanguage.isoKey]}
+                    />
+                  </div>
+                  <div>
+                    <FutureBoxCard
+                      title={phrases['signIn.futureBox.card.widgetApp.title'][defaultLanguage.isoKey]}
+                      description={phrases['signIn.futureBox.card.widgetApp.description'][defaultLanguage.isoKey]}
+                      imgSrc={phrases['signIn.futureBox.card.widgetApp.imageLink'][defaultLanguage.isoKey]}
+                      link={phrases['signIn.futureBox.card.widgetApp.link'][defaultLanguage.isoKey]}
+                    />
+                  </div>
+                  <div>
+                    <FutureBoxCard
+                      title={phrases['signIn.futureBox.card.workerApp.title'][defaultLanguage.isoKey]}
+                      description={phrases['signIn.futureBox.card.workerApp.description'][defaultLanguage.isoKey]}
+                      imgSrc={phrases['signIn.futureBox.card.workerApp.imageLink'][defaultLanguage.isoKey]}
+                      googleLink={phrases['signIn.futureBox.card.workerApp.playMarketLink'][defaultLanguage.isoKey]}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
             <Row>
               <Col
@@ -237,6 +269,41 @@ class SignIn extends Component {
       </div>
     );
   }
+}
+
+function FutureBoxCard({
+  imgSrc, title, description, appleLink, googleLink, link,
+}) {
+  return (
+    <div className="FutureBoxCard">
+      <div className="__Title">
+        <span>{title}</span>
+      </div>
+      <div className="__ImageBox">
+        <img src={imgSrc} alt={title} />
+      </div>
+      <div className="__Description">
+        <div dangerouslySetInnerHTML={{ __html: description }} />
+      </div>
+      <div className="__Links">
+        {appleLink && (
+          <a href={appleLink} target="_blank">
+            <Icon style={{ fontSize: 30, color: '#f77805' }} type="apple" />
+          </a>
+        )}
+        {googleLink && (
+          <a href={googleLink} target="_blank">
+            <Icon style={{ fontSize: 26, color: '#f77805' }} type="android" />
+          </a>
+        )}
+        {link && (
+          <a href={link} target="_blank">
+            <Icon style={{ fontSize: 26, color: '#f77805' }} type="chrome" />
+          </a>
+        )}
+      </div>
+    </div>
+  );
 }
 
 const { $setLanguage, $startApp, $getAppStatistic } = actions.app;
