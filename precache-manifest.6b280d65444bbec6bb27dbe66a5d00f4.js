e27e86260f5ed8427762e8ec1747ec98030425c2
Chabanov Pavel <businesseuropolicy@gmail.com>
2019-09-22 17:02:40 +0300

    Updates

---
+++ precache-manifest.6b280d65444bbec6bb27dbe66a5d00f4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e8aa2fc2ceb97008fed38a0ef200e4",
+    "url": "/Coupler_Business_Web/static/media/welcomeImage-mobile-step1.44e8aa2f.png"
+  },
+  {
+    "revision": "42d207b5b386e9b78394",
+    "url": "/Coupler_Business_Web/static/js/main.42d207b5.chunk.js"
+  },
+  {
+    "revision": "cca01eb53f1fda570e7ce1f22688abb5",
+    "url": "/Coupler_Business_Web/static/media/signIn_mcbookXL.cca01eb5.png"
+  },
+  {
+    "revision": "c2ffc3042f23212c1713",
+    "url": "/Coupler_Business_Web/static/js/runtime~main.c2ffc304.js"
+  },
+  {
+    "revision": "f2dc91e9e0f3066a20867628d6c4f5ac",
+    "url": "/Coupler_Business_Web/static/media/AddIcon.f2dc91e9.svg"
+  },
+  {
+    "revision": "78aba79d8f1b5226a6391f892631be39",
+    "url": "/Coupler_Business_Web/static/media/emptyState.78aba79d.svg"
+  },
+  {
+    "revision": "a350066884c50c80520bda486b27f995",
+    "url": "/Coupler_Business_Web/static/media/coupler_logo_beta.a3500668.svg"
+  },
+  {
+    "revision": "114f2331434d8c399f3ed976589465ff",
+    "url": "/Coupler_Business_Web/static/media/logo-single-white.114f2331.svg"
+  },
+  {
+    "revision": "caf7277804bd1d4e0fec4f18c356f7f6",
+    "url": "/Coupler_Business_Web/static/media/welcomeImage-step1.caf72778.png"
+  },
+  {
+    "revision": "08410840493a4cc7e66cb981b5b410cc",
+    "url": "/Coupler_Business_Web/static/media/signIn_mcbook.08410840.png"
+  },
+  {
+    "revision": "0b5cdd00b842150511733b27dc5e419c",
+    "url": "/Coupler_Business_Web/static/media/welcomeImage-step2.0b5cdd00.png"
+  },
+  {
+    "revision": "b2242a21c17b03b4c087afe7ff74937c",
+    "url": "/Coupler_Business_Web/static/media/welcomeImage-mobile-step2.b2242a21.png"
+  },
+  {
+    "revision": "691288702ed2e8b123f19b7571231fb1",
+    "url": "/Coupler_Business_Web/static/media/welcomeImage-step3.69128870.png"
+  },
+  {
+    "revision": "bf9baf8c6b81ab28193bbd976ac870f9",
+    "url": "/Coupler_Business_Web/static/media/welcomeImage-mobile-step3.bf9baf8c.png"
+  },
+  {
+    "revision": "0ae3d5e09a9c66ebc2ee246b5e735323",
+    "url": "/Coupler_Business_Web/static/media/logo-signIn.0ae3d5e0.svg"
+  },
+  {
+    "revision": "bca4e8eef7f7c1a4b6b441c21b4974ff",
+    "url": "/Coupler_Business_Web/static/media/signIn-tablet.bca4e8ee.svg"
+  },
+  {
+    "revision": "9624de7b21403febc086",
+    "url": "/Coupler_Business_Web/static/js/1.9624de7b.chunk.js"
+  },
+  {
+    "revision": "9624de7b21403febc086",
+    "url": "/Coupler_Business_Web/static/css/1.a758fa8b.chunk.css"
+  },
+  {
+    "revision": "3afb0061e8ec1a382c34c912e846752b",
+    "url": "/Coupler_Business_Web/index.html"
+  }
+];
